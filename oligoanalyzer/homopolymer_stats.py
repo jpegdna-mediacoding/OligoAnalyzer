3eda11bd15e396c692578bf2e02230f5017f98a9
xavpic <86108921+xavpic@users.noreply.github.com>
2023-03-14 10:51:44 +0100

    new_feature

--- oligoanalyzer/homopolymer_stats.py
+++ oligoanalyzer/homopolymer_stats.py
@@ -6,6 +6,8 @@ def compute_data(data):
     """Computes the data"""
     hom = []
     nuc_hom = []
+    hom_pos_b = []
+    hom_pos_e = []
     max_len = 4
     for line in data:
         if line[0] != ">":
@@ -14,6 +16,7 @@ def compute_data(data):
             i = 0
             while i < len(line)-3:
                 if line[i] == line[i+1] == line[i+2] == line[i+3]:
+                    hom_pos_b.append(i)
                     tmp_size = 0
                     tmp_hom += 1
                     j = i
@@ -22,6 +25,7 @@ def compute_data(data):
                         tmp_nuc += 1
                         i += 1
                     max_len = max(max_len, tmp_size)
+                    hom_pos_e.append(i-1)
                 else:
                     i+=1
             hom.append(tmp_hom)
@@ -35,25 +39,29 @@ def compute_data(data):
     print(f"Total number of homopolymers: {sum(hom)}")
     print(f"Overall average size of the homopolymers: {sum(nuc_hom)/sum(hom):.3f}")
     print(f"Maximum length of the homopolymers: {max_len}")
-    return (hom, nuc_hom, avg_hom)
+    return (hom, nuc_hom, avg_hom, hom_pos_b)
 
 
 def plot(stats):
     """Plot the stats on the data"""
-    hom_stats, nuc_hom_stats, avg_hom_stats = stats
-    fig, axs = plt.subplots(1, 3, sharey=True)
-    axs[0].hist([el for el in hom_stats if el != 0], 200)
-    axs[1].hist([el for el in nuc_hom_stats if el != 0], 200)
-    axs[2].hist([el for el in avg_hom_stats if el != 0], 200)
-    axs[0].set_title("Nb of homopolymers per oligo")
-    axs[0].set_xlabel("Nb of homopolymers")
-    axs[0].set_ylabel("Nb of oligos")
-    axs[1].set_title("Nb of nucleotides belgonging to a homopolymer per oligo")
-    axs[1].set_xlabel("Nb of nucleotides")
-    axs[1].set_ylabel("Nb of oligos")
-    axs[2].set_title("Avg homopolymer size per oligo")
-    axs[2].set_xlabel("Avg homopolymer size")
-    axs[2].set_ylabel("Nb of oligos")
+    hom_stats, nuc_hom_stats, avg_hom_stats, hom_pos_b = stats
+    fig, axs = plt.subplots(2, 2, sharey=False)
+    axs[0][0].hist([el for el in hom_stats if el != 0], 200)
+    axs[0][1].hist([el for el in nuc_hom_stats if el != 0], 200)
+    axs[1][0].hist([el for el in avg_hom_stats if el != 0], 200)
+    axs[1][1].hist(hom_pos_b, 500)
+    axs[0][0].set_title("Nb of homopolymers per oligo")
+    axs[0][0].set_xlabel("Nb of homopolymers")
+    axs[0][0].set_ylabel("Nb of oligos")
+    axs[0][1].set_title("Nb of nucleotides belgonging to a homopolymer per oligo")
+    axs[0][1].set_xlabel("Nb of nucleotides")
+    axs[0][1].set_ylabel("Nb of oligos")
+    axs[1][0].set_title("Avg homopolymer size per oligo")
+    axs[1][0].set_xlabel("Avg homopolymer size")
+    axs[1][0].set_ylabel("Nb of oligos")
+    axs[1][1].set_title("Homopolymers positions in the oligo")
+    axs[1][1].set_xlabel("Nucleotide")
+    axs[1][1].set_ylabel("Nb of homopolymers")
     plt.show()
 
 
