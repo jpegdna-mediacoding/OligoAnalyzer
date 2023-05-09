f94fe56703c42f33dece91c1b0ac026ca742596c
xavpic <86108921+xavpic@users.noreply.github.com>
2023-05-09 16:52:26 +0200

    update01

--- oligoanalyzer/pattern_stats.py
+++ oligoanalyzer/pattern_stats.py
@@ -1,7 +1,10 @@
 import argparse
+import numpy as np
+import matplotlib.pyplot as plt
 from tools.fasta import get_oligos
 
 PATTERN_SIZE_RANGE = (3,5)
+LEN_PATTERN_SIZE_RANGE = PATTERN_SIZE_RANGE[1] - PATTERN_SIZE_RANGE[0] + 1
 
 def compute_data(data):
     """Compute simple pattern statistics on the oligo pool"""
@@ -12,11 +15,11 @@ def compute_data(data):
         res = detect_patterns(oligo)
         pattern_counts.append(res[0])
         pattern_infos.append(res[1])
-    return sum(pattern_counts)
+    return pattern_counts
 
 def detect_patterns(oligo):
     """Detect the patterns in a specific oligo"""
-    pattern_counts, pattern_info = 0, {}
+    pattern_counts, pattern_info = [0, 0, 0], {}
     mask = [0] * len(oligo)
     for length in range(PATTERN_SIZE_RANGE[1], PATTERN_SIZE_RANGE[0]-1, -1):
         j = 0
@@ -29,13 +32,13 @@ def detect_patterns(oligo):
                     update_pattern_info(pattern_info[pattern], new_info)
                 else:
                     pattern_info[pattern] = new_info
-                pattern_counts += 3
+                pattern_counts[length-PATTERN_SIZE_RANGE[0]] += 3
                 mask[j:j+3*length] = [1]*3*length
                 j += 3*length
                 while (j+length < len(oligo) and oligo[j:j+length] == pattern and mask[j:j+length]):
                     new_info = {'position': {(j, j+length)}, 'population': 1}
                     update_pattern_info(pattern_info[pattern], new_info)
-                    pattern_counts += 1
+                    pattern_counts[length-PATTERN_SIZE_RANGE[0]] += 1
                     mask[j:j+length] = [1]*length
                     j += length
             else:
@@ -48,6 +51,29 @@ def update_pattern_info(info, new_info):
     info['population'] += new_info['population']
     return info
 
+def plot(stats):
+    """Plot the stats on the data"""
+    plt.figure()
+    data = [[sum(el) for el in stats if sum(el)!=0]]
+    data2 = []
+    legend = ["General"]
+    for i in range(LEN_PATTERN_SIZE_RANGE):
+        data.append([el[i] for el in stats if el[i]!=0])
+        data2.append(sum(data[-1]))
+        legend.append(f"length={PATTERN_SIZE_RANGE[0]+i}")
+    plt.subplot(121)
+    plt.hist(data, bins=np.arange(3, max(data[0]), 1), label=legend)
+    plt.legend(loc="upper right")
+    plt.title("Nb of patterns per oligo")
+    plt.xlabel("Nb of patterns")
+    plt.ylabel("Nb of oligos")
+    plt.subplot(122)
+    plt.bar([str(el2) for el2 in range(PATTERN_SIZE_RANGE[0], PATTERN_SIZE_RANGE[1]+1)], data2)
+    plt.title("Nb of patterns per oligo")
+    plt.xlabel("Nb of patterns")
+    plt.ylabel("Nb of oligos")
+    plt.show()
+
 def main():
     """Plots statistics on the patterns over all the oligos"""
     parser = argparse.ArgumentParser()
@@ -57,8 +83,9 @@ def main():
     args = parser.parse_args()
     fname = args.FNAME
     data = get_oligos(fname)
-    n_patterns = compute_data(data)
-    print(f'Number of patterns: {n_patterns}')
+    stats = compute_data(data)
+    print(f'Number of patterns: {sum([sum(el) for el in stats])}')
+    plot(stats)
 
 if __name__ == '__main__':
     main()
